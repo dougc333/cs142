f1769436aa56441d0e8ae8d65761110a533b8124
dougc333 <124989+dougc333@users.noreply.github.com>
2020-05-06 23:42:53 -0700

    add

--- project4/p5.jsx
+++ project4/p5.jsx
@@ -1,16 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { HashRouter, Route, Link } from "react-router-dom";
+
+import DynamicView from './components/dynamic/DynamicView';
 import Header from './components/header/Header';
-//import DynamicView from './components/dynamic/DynamicView';
-//import './DynamicView.css';
-
-
+import './components/dynamic/DynamicView.css';
 
 ReactDOM.render(
-  <div>
-    <Header />
-    <DynamicView />
-  </div>,
+    <div>
+      <Header />
+      <DynamicView />
+    </div>,
   document.getElementById('reactapp'),
 );
